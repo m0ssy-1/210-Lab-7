8f4d0ab3b535b3576c741e54458f75acdff7abe0
Phillip Wagner <m0ssyblue7@gmail.com>
2025-09-14 19:12:58 -0700

    Fourth addition to dynamic.arrays.cpp

--- dynamic.arrays.cpp
+++ dynamic.arrays.cpp
@@ -32,3 +32,12 @@ delete[] arr;
 
 return 0;
 }
+
+string* reverseArray(string* arr, int size) {
+    for (int i = 0; i < size /2; i++) {
+        string temp = *(arr + i);
+        *(arr + i) = *(arr + (size - 1 - i));
+        *(arr + (size - 1 - i)) = temp;
+    }
+    return arr;
+}
