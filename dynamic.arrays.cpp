ee3422014c4076dd0454dfcbfc3bbd7711641dda
Phillip Wagner <m0ssyblue7@gmail.com>
2025-09-14 18:48:33 -0700

    Third addition to dynamic.arrays.cpp

--- dynamic.arrays.cpp
+++ dynamic.arrays.cpp
@@ -4,15 +4,31 @@ using namespace std;
 
 const int SIZE = 5;  // const array size
 
- 
+//funct protos
+string* reverseArray(string* arr, int size);
+void displayArray(string* arr, int size);
 
 int main(){
 
-    string arr = new string[SIZE];
+    string* arr = new string[SIZE];
 
+//array names
     *(arr + 0)= "Janet";
     *(arr + 1) = "Jeffe";
     *(arr + 2)= "Jin";
     *(arr + 3) = "Joe";
     *(arr + 4) = "Junio";
+
+//display original array
+cout << "something";
+displayArray(arr, SIZE);
+
+reverseArray(arr, SIZE);
+
+cout << "something";
+displayArray(arr, SIZE);
+
+delete[] arr;
+
+return 0;
 }
