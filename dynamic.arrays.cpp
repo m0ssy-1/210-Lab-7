221e777ac8c62860f78ff9cc90e6d0a9b1e16aaf
Phillip Wagner <m0ssyblue7@gmail.com>
2025-09-14 18:18:58 -0700

    Second addition to dynamic.arrays.cpp

--- dynamic.arrays.cpp
+++ dynamic.arrays.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+const int SIZE = 5;  // const array size
+
+ 
+
+int main(){
+
+    string arr = new string[SIZE];
+
+    *(arr + 0)= "Janet";
+    *(arr + 1) = "Jeffe";
+    *(arr + 2)= "Jin";
+    *(arr + 3) = "Joe";
+    *(arr + 4) = "Junio";
+}
